b68f7cf629acb791529a44e181386caba9272990
Ziao Huang <71763696+amazing-fish@users.noreply.github.com>
2025-12-31 18:03:11 +0800

    v0.4.10 feature: 详情页周末日期高亮

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ from datetime import datetime, time as dtime
 # 配置区域 (Configuration)
 # ==========================================
 class Config:
-    APP_VERSION = "v0.4.9"
-    APP_VERSION_TYPE = "bugfix"
+    APP_VERSION = "v0.4.10"
+    APP_VERSION_TYPE = "feature"
 
     # —— 会被首次配置覆盖的参数（默认值）——
     MONTHLY_SALARY = 20000.0
